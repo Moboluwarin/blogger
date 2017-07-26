d5a0e9b32a103d43702a4bf9bf5f2f51ae2a3cbb
Moboluwarin <kolaogunnaike@gmail.com>
2017-07-26 13:14:25 +0200

    created comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,12 +38,15 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
   private
+
   def post_params
     params.require(:post).permit(:title, :body)
   end
 
-  def find_post
-    @post = Post.find(params[:id])
-  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+Comment.destroy_all
 Post.destroy_all
-post = Post.create!([{title:"Ever Green", body:"An ever green stage along side state"},{title:"Vegetations",
-          body:"An ever green stage along side state"}]);
+post1 = Post.create!(title:"Ever Green", body:"An ever green stage along side state")
+post2 = Post.create!(title:"Vegetations", body:"An ever green stage along side state")
+comment = Comment.create!(name:"Kola", body:"I love greens", post:post1)
